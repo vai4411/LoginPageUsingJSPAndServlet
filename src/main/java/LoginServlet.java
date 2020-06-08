a0a626a2e5c332601edf8a0258cad55e5cfd428d
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-08 15:46:57 +0530

    ADDED: Checked User Name Valid Or Not

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -4,13 +4,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.util.regex.Pattern;
+
+import static java.lang.String.valueOf;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
+    public Pattern namePattern;
+    public Pattern passwordPattern;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String userName = request.getParameter("name");
         String password = request.getParameter("password");
-        if (userName.equals("Vaibhav") && password.equals("Vaibhav@123"))
+        namePattern = namePattern.compile("^[A-Z][A-Za-z]{2,}");
+        if (namePattern.matches(valueOf(namePattern),userName) && userName.equals("Vaibhav") && password.equals("Vaibhav@123"))
             response.sendRedirect("welcome.jsp");
     }
 }
